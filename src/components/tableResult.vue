1749ea9ab88ef72baf23ba98f59a7c895540dfdb
jh8057 <luckysymbol13@gmail.com>
2022-10-22 15:37:33 +0900

    null

--- src/components/tableResult.vue
+++ src/components/tableResult.vue
@@ -57,7 +57,7 @@ export default {
       this.result2.forEach((el: any) => {
         if (el.answer === el.selected) correctAnswer++;
       });
-      return correctAnswer;
+      return correctAnswer || "";
     },
   },
 };
